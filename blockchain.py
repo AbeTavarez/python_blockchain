ae3fddc6131e12eb594f0518c116f9e7dc56bc42
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-04 16:29:23 -0500

    block

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
-blockchain = [[1]]
+blockchain = []
 
 
-def add_value(transaction_amount):
-    blockchain.append([blockchain[-1], transaction_amount])
-    print(blockchain)
+def get_last_bc_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([transaction_amount, last_transaction])
 
 
 add_value(3)
+add_value(8, get_last_bc_value())
+add_value(11, get_last_bc_value())
+
+print(blockchain)
