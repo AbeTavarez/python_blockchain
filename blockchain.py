e1e89b8ea31e3014a9472ee2d0cb208a635818f3
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-29 23:45:59 -0500

    main logic

--- blockchain.py
+++ blockchain.py
@@ -46,12 +46,15 @@ while True:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('q: To quit.')
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
         add_value(tx_amount, get_last_bc_value())
     elif user_choice == 2:
         print_blockchain_elements()
+    elif user_choice == 'q':
+        break
     else:
         print('Input was invalid, please pick a value from the list!')
 
