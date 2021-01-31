6cd20d617f9270cac20ad028765ff22d216b0423
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-30 23:30:25 -0500

    hash block

--- blockchain.py
+++ blockchain.py
@@ -11,12 +11,14 @@ def get_last_bc_value():
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     """ Append a new value to the back of the blockchain.
     Arguments:
       :transaction_amount: The amount that should be added.
       :last_transaction: The last blockchain transaction (default [1])
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -40,21 +42,21 @@ def print_blockchain_elements():
 #* Main ####################
 
 
-tx_amount = get_transaction_value()
-add_value(tx_amount)
-
-
 while True:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('h: manipulate the chain')
     print('q: To quit.')
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_bc_value())
+        add_transaction(tx_amount, get_last_bc_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == 'q':
         break
     else:
