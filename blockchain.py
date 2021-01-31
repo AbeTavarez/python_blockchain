5ff36cb6db27fe4406eaae0d28ea867e351bb040
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-31 00:07:03 -0500

    block verification

--- blockchain.py
+++ blockchain.py
@@ -38,6 +38,8 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+    else:
+        print('-' * 20)
 
 
 def verify_chain():
@@ -56,10 +58,11 @@ def verify_chain():
         block_index += 1
     return is_valid
 
+
 #* Main ####################
+waiting_for_input = True
 
-
-while True:
+while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
@@ -75,12 +78,14 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
     if not verify_chain():
         print('Invalid Blockchain')
         break
     print('Choice registred!')
+else:
+    print('User left')
 
 print('Done!')
